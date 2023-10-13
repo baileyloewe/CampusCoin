13d41d9aa2de9d59726dc00c87be7475dffc9137
wroach24 <willroach24@gmail.com>
2023-10-12 21:57:57 -0400

    Create EmailValidationAttribute.cs

---
+++ CampusCoin/Validation/EmailValidationAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CampusCoin.Validation
+{
+    public class EmailValidationAttribute : ValidationAttribute
+    {
+        private readonly string _emailRegexPattern = @"^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,6}$";
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                return new ValidationResult("Email address is required");
+            }
+
+            string email = value.ToString();
+
+            if (!Regex.IsMatch(email, _emailRegexPattern))
+            {
+                return new ValidationResult("Invalid email address format");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
