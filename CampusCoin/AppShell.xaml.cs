0ed1533c6a9f16d33aa5b0f0aff9e8142d31ad08
bloewe42 <bloewe@ymail.com>
2023-10-17 12:41:50 -0400

    Edited Registration Page
    Mostly finished registration page. Still working one on issue with registration.
    
    Updated username to email

--- CampusCoin/AppShell.xaml.cs
+++ CampusCoin/AppShell.xaml.cs
@@ -10,5 +10,6 @@ public partial class AppShell : Shell
         InitializeComponent();
         Routing.RegisterRoute(nameof(LoginPage), typeof(LoginPage));
         Routing.RegisterRoute(nameof(RegistrationPage), typeof(RegistrationPage));
+        Routing.RegisterRoute(nameof(MainPage), typeof(MainPage));
     }
 }
--- CampusCoin/Models/Users.cs
+++ CampusCoin/Models/Users.cs
@@ -11,20 +11,21 @@ public partial class Users
 
     [Required]
     [StringLength(50)]
-    public string Username { get; set; }
+    public string Email { get; set; }
 
     [Required]
     [StringLength(50)]
     public string Password { get; set; }
 
-    public string Email { get; set; }
-
+    [Required]
     [StringLength(50)]
     public string PhoneNumber { get; set; }
 
+    [Required]
     [StringLength(50)]
     public string FirstName { get; set; }
 
+    [Required]
     [StringLength(50)]
     public string LastName { get; set; }
 }
