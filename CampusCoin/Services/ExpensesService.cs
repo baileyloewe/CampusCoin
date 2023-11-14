33b6fd238cfadc3293b1b826b0cfd2948ab6a10e
aSilvaCodes <103013501+aSilvaCodes@users.noreply.github.com>
2023-11-14 18:40:00 -0500

    submitted UserData to DB
    submission of UserData to db now functional but limited to hardcoded data at the moment

--- CampusCoin/Services/ExpensesService.cs
+++ CampusCoin/Services/ExpensesService.cs
@@ -3,12 +3,20 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CampusCoin.Services
 {
-    class ExpensesService
+    public class ExpensesService
     {
         private IDbContextFactory<CampusCoinContext> _context;
+        public string DbPath { get; }
         public ExpensesService(IDbContextFactory<CampusCoinContext> context)
         {
             _context = context;
+        }
+
+        public async Task SubmitExpense(UserData userData)
+        {
+            var dbContext = await _context.CreateDbContextAsync(); // Create database context
+            dbContext.UserData.Add(userData); // Add user data to the database
+            dbContext.SaveChanges(); // Save the changes to the database
         }
     }
 }
